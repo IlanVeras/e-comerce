e1d7e9cecf8e531e23aa6a32318e8e4cc7bee6f9
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-10-16 18:03:32 -0400

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
 import Navbar from './Components/NavBar/Navbar'
+import Shop from "./Pages/Shop"
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import LoginSingup from './Pages/LoginSingup'
 
 function App() {
   return (
     <div>
-       <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory category="men"/>}/>
+          <Route path='/womans' element={<ShopCategory category="womans"/>}/>
+          <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSingup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- frontend/src/Components/NavBar/Navbar.jsx
+++ frontend/src/Components/NavBar/Navbar.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
-import logo from "../Assets/logo.png"
-import cart_icon from "../Assets/cart_icon.png"
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-    //estado da barra vermelha
-    const [menu,setMenu] = React.useState('shop')
+  //estado da barra vermelha
+  const [menu, setMenu] = React.useState("shop");
 
-    //function that changes the menu and allows other menus to have the under red bar
-    function handleMenu(name){
-        setMenu(name)
-    }
+  //function that changes the menu and allows other menus to have the under red bar
+  function handleMenu(name) {
+    setMenu(name);
+  }
   return (
-    <div className='navbar'>
-      <div className='nav-logo'>
+    <div className="navbar">
+      <div className="nav-logo">
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className='nav-menu'>
-        <li onClick={() => handleMenu('shop')}>Shop {menu === 'shop' ? <hr></hr> : <></>}</li>
-        <li onClick={() => handleMenu('mens')}>Man {menu === 'mens' ? <hr></hr> : <></>}</li>
-        <li onClick={() => handleMenu('womans')}>Woman {menu === 'womans' ? <hr></hr> : <></>}</li>
-        <li onClick={() => handleMenu('kids')}>Kids {menu === 'kids' ? <hr></hr> : <></>}</li>
+      <ul className="nav-menu">
+        <li onClick={() => handleMenu("shop")}>
+          <Link style={{textDecoration: 'none'}} to="/">Shop</Link> {menu === "shop" ? <hr></hr> : <></>}
+        </li>
+        <li onClick={() => handleMenu("mens")}>
+          <Link style={{textDecoration: 'none'}} to="/mens">Man</Link>
+          {menu === "mens" ? <hr></hr> : <></>}
+        </li>
+        <li onClick={() => handleMenu("womans")}>
+          <Link style={{textDecoration: 'none'}} to="/womans">Woman</Link>
+          {menu === "womans" ? <hr></hr> : <></>}
+        </li>
+        <li onClick={() => handleMenu("kids")}>
+          <Link style={{textDecoration: 'none'}} to="/kids">Kids</Link>
+          {menu === "kids" ? <hr></hr> : <></>}
+        </li>
       </ul>
-      <div className='nav-login-cart'>
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+      <div className="nav-login-cart">
+        <Link style={{textDecoration: 'none'}} to="/login">
+          <button onClick={() => handleMenu("login")}>Login</button>
+        </Link>
+        <Link style={{textDecoration: 'none'}} to="/cart">
+          <img src={cart_icon} alt="" onClick={() => handleMenu("cart")} />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
