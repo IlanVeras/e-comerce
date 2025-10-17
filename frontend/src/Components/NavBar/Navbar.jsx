6928cd6e86572ffd25655c856256c84563032646
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-10-17 15:15:07 -0400

    Hero for Shop page

--- frontend/src/Components/NavBar/Navbar.jsx
+++ frontend/src/Components/NavBar/Navbar.jsx
@@ -20,26 +20,35 @@ const Navbar = () => {
       </div>
       <ul className="nav-menu">
         <li onClick={() => handleMenu("shop")}>
-          <Link style={{textDecoration: 'none'}} to="/">Shop</Link> {menu === "shop" ? <hr></hr> : <></>}
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr></hr> : <></>}
         </li>
         <li onClick={() => handleMenu("mens")}>
-          <Link style={{textDecoration: 'none'}} to="/mens">Man</Link>
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Man
+          </Link>
           {menu === "mens" ? <hr></hr> : <></>}
         </li>
         <li onClick={() => handleMenu("womans")}>
-          <Link style={{textDecoration: 'none'}} to="/womans">Woman</Link>
+          <Link style={{ textDecoration: "none" }} to="/womans">
+            Woman
+          </Link>
           {menu === "womans" ? <hr></hr> : <></>}
         </li>
         <li onClick={() => handleMenu("kids")}>
-          <Link style={{textDecoration: 'none'}} to="/kids">Kids</Link>
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
           {menu === "kids" ? <hr></hr> : <></>}
         </li>
       </ul>
       <div className="nav-login-cart">
-        <Link style={{textDecoration: 'none'}} to="/login">
+        <Link style={{ textDecoration: "none" }} to="/login">
           <button onClick={() => handleMenu("login")}>Login</button>
         </Link>
-        <Link style={{textDecoration: 'none'}} to="/cart">
+        <Link style={{ textDecoration: "none" }} to="/cart">
           <img src={cart_icon} alt="" onClick={() => handleMenu("cart")} />
         </Link>
         <div className="nav-cart-count">0</div>
